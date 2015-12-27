68c88178f09bf20777172596e592367b976d1c22
Julien Viet <julien@julienviet.com>
2015-12-27 12:32:09 +0100

    Update example

--- scripts/simple_handler.js
+++ scripts/simple_handler.js
@@ -1,6 +1,6 @@
 var MessageRouter = require("nonobot-js/message_router.js");
 
-var router = MessageRouter.create(vertx);
-router.handler().respond("hello", function(message) {
+var router = MessageRouter.getShared(vertx);
+router.respond("hello", function(message) {
   message.reply("hello sir, how are you doing today?");
-}).create();
+});
